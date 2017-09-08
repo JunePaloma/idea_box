3379ced194f14307fd3d733cc3b5cabb0b63acf1
JunePaloma <kborkoski@gmail.com>
2017-09-07 19:04:51 -0600

    Add sad path testing for creating a log-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, presence: true, uniqueness: true, confirmation: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
 
   enum role: [:default, :admin]
 end
--- spec/features/user_can_create_a_log_in_spec.rb
+++ spec/features/user_can_create_a_log_in_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "A guest visits the root " do
     context "and sees a link to create a new user" do
     context "clicking on that link brings the visitor to the New user page" do
-      it "fills out a form and is able to create a user profile" do
+      it "fills out a form with valid information and is able to create a user profile" do
 
         visit "/"
         expect(page).to have_link("Create New User")
@@ -17,6 +17,51 @@ RSpec.describe "A guest visits the root " do
 
         expect(current_path).to eq(user_path(User.last))
         expect(page).to have_content ("Hello Bowser!")
+      end
+
+      it "fills out a form with a user name that's taken and is not able to create a user profile" do
+
+        visit "/"
+        create(:user)
+        expect(page).to have_link("Create New User")
+        click_on "Create New User"
+        expect(current_path).to eq(new_user_path)
+
+        fill_in "user[username]", with: "Bowser"
+        fill_in "user[password]", with: "woof"
+
+        click_on "Create User"
+
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it "fills out a form with a user name that's taken and is not able to create a user profile" do
+
+        visit "/"
+        create(:user)
+        expect(page).to have_link("Create New User")
+        click_on "Create New User"
+        expect(current_path).to eq(new_user_path)
+
+        fill_in "user[username]", with: "Bowser"
+        fill_in "user[password]", with: "woof"
+
+        click_on "Create User"
+
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it "fills out a form with a user name that's taken and is not able to create a user profile" do
+
+        visit "/"
+
+        expect(page).to have_link("Create New User")
+        click_on "Create New User"
+        expect(current_path).to eq(new_user_path)
+
+        fill_in "user[username]", with: "Bowser"
+        click_on "Create User"
+        expect(current_path).to eq(new_user_path)
       end
     end
   end
